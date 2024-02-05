df360065f1199e02c9f973f461184ee13b16a7bd
MohamedJamh <jam7.mohamed@gmail.com>
2024-02-05 16:40:43 +0100

    main - add : setup

---
+++ auth0-server/src/test/java/com/auth0/Auth0ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth0ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
